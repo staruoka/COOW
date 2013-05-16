e950dfe86da0a7710de84c5390d820b5ed79faf8
Cawae <heeekn116@gmail.com>
2013-05-16 18:37:56 +0900

    CooWスクロールでないVer
    確認用に登録します

---
+++ theCooW2/index.js
@@ -0,0 +1,21 @@
+﻿function adjustImage(image, time) {
+	var image_width = image.find("img").width();
+	var image_height = image.find("img").height();
+	var width = 0;
+	var height = 0;
+	var margin_left = 0;
+	var margin_top = 0;
+	var ratio_outer = $(window).width()/$(window).height();
+	var ratio_inner = image_width/image_height;
+	if (ratio_outer > ratio_inner) {
+		width = $(window).width();
+		height = width/ratio_inner;
+	} else {
+		height = $(window).height();
+		width = height*ratio_inner;
+	}
+	margin_left = Math.floor(($(window).width()-width)/2);
+	margin_top = Math.floor(($(window).height()-height)/2);
+	image.find("img").animate({width:width, height:height}, {duration:time, easing:"easeOutQuart"});
+	image.animate({top:margin_top, left:margin_left}, {duration:time, easing:"easeOutQuart"});
+}
